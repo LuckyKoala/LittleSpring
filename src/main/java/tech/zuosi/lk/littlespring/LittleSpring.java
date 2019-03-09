bd5409352a851bc8774158054b6f0b6af758036d
LuckyKoala <pilotcraft.lk@gmail.com>
2019-03-09 19:54:27 +0800

    extract ApplicationContext class

--- src/main/java/tech/zuosi/lk/littlespring/LittleSpring.java
+++ src/main/java/tech/zuosi/lk/littlespring/LittleSpring.java
@@ -1,5 +1,6 @@
 package tech.zuosi.lk.littlespring;
 
+import tech.zuosi.lk.littlespring.context.ApplicationContext;
 import tech.zuosi.lk.littlespring.exception.InvalidBeanException;
 import tech.zuosi.lk.littlespring.exception.InvalidConfigurationException;
 import tech.zuosi.lk.littlespring.exception.InvalidPackageException;
@@ -15,42 +16,17 @@ import java.lang.annotation.Annotation;
  * Created by luckykoala on 19-2-28.
  */
 public class LittleSpring {
-    private BeanFactory beanFactory = new BeanFactory();
+    private ApplicationContext context = new ApplicationContext();
 
     public LittleSpring(Class clazz)
             throws IllegalAccessException, InvalidBeanException,
             InvalidConfigurationException, InstantiationException,
             NoSuitableConstructorForComponentException, InvalidPackageException {
 
-        readConfig(clazz);
+        context.readConfig(clazz);
     }
 
     public Object getBean(String beanId) throws Exception {
-        return beanFactory.getBean(beanId);
-    }
-
-    private void readConfig(Class clazz)
-            throws InvalidBeanException, IllegalAccessException, InstantiationException,
-            InvalidConfigurationException, NoSuitableConstructorForComponentException, InvalidPackageException {
-        //尝试读取传入的配置类
-        if(clazz.isAnnotationPresent(Configuration.class)) beanFactory.readConfig(clazz);
-        //读取导入的配置类
-        for(Annotation annotation : clazz.getAnnotationsByType(Import.class)) {
-            Class configClazz = ((Import) annotation).value();
-            //导入的配置类应该使用@Configuration标注
-            if(!configClazz.isAnnotationPresent(Configuration.class))
-                throw new InvalidConfigurationException(configClazz.getName());
-            beanFactory.readConfig(configClazz);
-        }
-        //尝试自动扫描
-        if(clazz.isAnnotationPresent(ComponentScan.class)) {
-            ComponentScan componentScan = (ComponentScan) clazz.getAnnotation(ComponentScan.class);
-            String[] basePackages = componentScan.basePackages();
-            if(basePackages.length != 0) {
-                for(String str : basePackages) beanFactory.autoScan(str);
-            } else {
-                beanFactory.autoScan(clazz.getPackage().getName());
-            }
-        }
+        return context.getBean(beanId);
     }
 }
---
+++ src/main/java/tech/zuosi/lk/littlespring/context/ApplicationContext.java
@@ -0,0 +1,51 @@
+package tech.zuosi.lk.littlespring.context;
+
+import tech.zuosi.lk.littlespring.exception.InvalidBeanException;
+import tech.zuosi.lk.littlespring.exception.InvalidConfigurationException;
+import tech.zuosi.lk.littlespring.exception.InvalidPackageException;
+import tech.zuosi.lk.littlespring.exception.NoSuitableConstructorForComponentException;
+import tech.zuosi.lk.littlespring.ioc.BeanFactory;
+import tech.zuosi.lk.littlespring.ioc.annotation.ComponentScan;
+import tech.zuosi.lk.littlespring.ioc.annotation.Configuration;
+import tech.zuosi.lk.littlespring.ioc.annotation.Import;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Created by luckykoala on 19-3-9.
+ */
+public class ApplicationContext {
+    private BeanFactory beanFactory = new BeanFactory();
+
+    public ApplicationContext() {}
+
+    public Object getBean(String beanId) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        return beanFactory.getBean(beanId);
+    }
+
+    public void readConfig(Class clazz)
+            throws InvalidBeanException, IllegalAccessException, InstantiationException,
+            InvalidConfigurationException, NoSuitableConstructorForComponentException, InvalidPackageException {
+        //尝试读取传入的配置类
+        if(clazz.isAnnotationPresent(Configuration.class)) beanFactory.readConfig(clazz);
+        //读取导入的配置类
+        for(Annotation annotation : clazz.getAnnotationsByType(Import.class)) {
+            Class configClazz = ((Import) annotation).value();
+            //导入的配置类应该使用@Configuration标注
+            if(!configClazz.isAnnotationPresent(Configuration.class))
+                throw new InvalidConfigurationException(configClazz.getName());
+            beanFactory.readConfig(configClazz);
+        }
+        //尝试自动扫描
+        if(clazz.isAnnotationPresent(ComponentScan.class)) {
+            ComponentScan componentScan = (ComponentScan) clazz.getAnnotation(ComponentScan.class);
+            String[] basePackages = componentScan.basePackages();
+            if(basePackages.length != 0) {
+                for(String str : basePackages) beanFactory.autoScan(str);
+            } else {
+                beanFactory.autoScan(clazz.getPackage().getName());
+            }
+        }
+    }
+}
